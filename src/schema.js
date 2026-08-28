51aba2c531f0171e59810df2eea7f57cd5305321
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-08-28 11:02:32 +0100

    Sonar fixes

--- src/schema.js
+++ src/schema.js
@@ -1,22 +1,26 @@
 import Joi from "joi";
 
+const MAX_30 = 30;
+const MAX_50 = 50;
+const MAX_120 = 120;
+
 const accountId = Joi.alternatives()
-  .try(Joi.string().max(50), Joi.number())
+  .try(Joi.string().max(MAX_50), Joi.number())
   .custom(String);
 
 export const eventSchema = Joi.object({
-  user: Joi.string().max(50).allow(""),
-  sessionId: Joi.string().max(50).allow(""),
-  correlationId: Joi.string().max(50).required(),
+  user: Joi.string().max(MAX_50).allow(""),
+  sessionId: Joi.string().max(MAX_50).allow(""),
+  correlationId: Joi.string().max(MAX_50).required(),
   datetime: Joi.date().iso().required(),
   version: Joi.string().max(10).required(),
-  application: Joi.string().max(30).required(),
-  service: Joi.string().max(30).required(),
+  application: Joi.string().max(MAX_30).required(),
+  service: Joi.string().max(MAX_30).required(),
   eventData: Joi.object({
     accounts: Joi.object({
       sbi: accountId,
     }).default({}),
-    status: Joi.string().max(120).allow(""),
+    status: Joi.string().max(MAX_120).allow(""),
     details: Joi.object().default({}),
   }).required(),
 }).required();
@@ -28,6 +32,6 @@ export const configSchema = Joi.object({
   }).required(),
   version: Joi.string().max(10),
   generateCorrelationId: Joi.boolean(),
-  service: Joi.string().max(30),
-  application: Joi.string().max(30),
+  service: Joi.string().max(MAX_30),
+  application: Joi.string().max(MAX_30),
 }).required();
